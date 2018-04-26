e644f2e8182995b8c66c564164a0bb6b00df3774
Billy Vong <billy@sentry.io>
2018-04-26 14:33:29 -0700

    fix(ui): Avatar should never shrink when inside flexbox

--- src/sentry/static/sentry/app/components/avatar/baseAvatar.jsx
+++ src/sentry/static/sentry/app/components/avatar/baseAvatar.jsx
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import React from 'react';
 import classNames from 'classnames';
 import qs from 'query-string';
+import styled from 'react-emotion';
 
 import ConfigStore from '../../stores/configStore';
 import LetterAvatar from '../letterAvatar';
@@ -126,7 +127,7 @@ class BaseAvatar extends React.Component {
 
     return (
       <Tooltip title={tooltip} disabled={!hasTooltip}>
-        <span
+        <StyledBaseAvatar
           className={classNames('avatar', this.props.className)}
           style={{
             ...sizeStyle,
@@ -135,10 +136,14 @@ class BaseAvatar extends React.Component {
         >
           {this.state.showBackupAvatar && this.renderLetterAvatar()}
           {this.renderImg()}
-        </span>
+        </StyledBaseAvatar>
       </Tooltip>
     );
   }
 }
 
 export default BaseAvatar;
+
+const StyledBaseAvatar = styled('span')`
+  flex-shrink: 0;
+`;
